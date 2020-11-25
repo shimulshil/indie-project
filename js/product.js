12d6b2d1d3c6f897205151d3dd0ca5fbad0b01f8
shimulshil <72187771+shimulshil@users.noreply.github.com>
2020-11-25 13:37:28 +0100

    Add files via upload

--- js/product.js
+++ js/product.js
@@ -1,44 +1,38 @@
-// let zoo = document.getElementById("zoo");
 let form = document.getElementById("form");
 let button = document.getElementById("submit");
 let myPictureTag = document.getElementById("product-img");
 
 form.addEventListener("submit", createAnimal);
 
-zoo.addEventListener("click", clickHandler);
+// zoo.addEventListener("click", clickHandler);
 
 function createAnimal (event){
     event.preventDefault();
 
-    let name = document.getElementById("input-kategori").value;
+    // let name = document.getElementById("input-kategori").value;
+    
     let product = document.getElementById("input-product").value;
-    let price = document.getElementById("input-price").value;
+    
+    let index = document.getElementById("input-product").selectedIndex
+    
+    let descId = document.getElementById("input-product")[index].dataset.desc;
+    
+    let desc = document.getElementById(descId).innerHTML
+
+    // let price = document.getElementById("input-price").value;
+
+    let descId1 = document.getElementById("input-product")[index].dataset.price;
+
+    let prices = document.getElementById(descId1).innerHTML
 
     myPictureTag.setAttribute("src", "product-img1/"+product+".png"); 
-    let html = ``;
-    
-    let html1 = `
-    <div>${name}</div>  
-    <div><span>${product}</span></div> 
-    `;
-    let html2 = `
-    <div><span>${price}</span></div>   
-    `;
+      
+    let html1 = `<p>${desc}</p>`;
+    let html2 = `<div><span>${prices}</span></div>`;
 
-    //zoo.innerHTML = html;
-    zoo1.innerHTML = html1;
-    zoo2.innerHTML = html2;
+    productdescription.innerHTML = html1;
+    productprice.innerHTML = html2;
     form.reset();
-    
-    // form.style.display = "none";
-}
-
-function clickHandler (event){
-    
-        // if(event.target.classList.contains("zoo")){
-        
-        // form.style.display="block";
-        // form.style.display="none";
-    // }
 
 }
+
